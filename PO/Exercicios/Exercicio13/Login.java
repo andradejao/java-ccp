cab1eb9eb0713b4381dbc3b7693016e08c864291
João Vitor Andrade <129785228+andradejao@users.noreply.github.com>
2026-04-26 10:12:24 -0300

    Create Login.java

---
+++ PO/Exercicios/Exercicio13/Login.java
@@ -0,0 +1,40 @@
+package testalogin;
+
+public class Login {
+
+    private String nome;
+    private String acesso;
+    private String password;
+
+    private String[][] users = {
+        {"marco", "123", "admin"},
+        {"luiza", "123", "enfermeira"},
+        {"luka", "123", "médico"}
+    };
+
+    public Login() {
+    }
+
+    public Login(String nome, String password) {
+        this.nome = nome;
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getAcesso() {
+        return acesso;
+    }
+
+    public boolean autentica() {
+        for (int i = 0; i < users.length; i++) {
+            if (users[i][0].equals(nome) && users[i][1].equals(password)) {
+                acesso = users[i][2];
+                return true;
+            }
+        }
+        return false;
+    }
+}
